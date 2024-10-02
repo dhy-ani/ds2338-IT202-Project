f3ffe33ba3dc2203e181a39a919554e6eb477e10
dhy-ani <dhyanisoni77@gmail.com>
2024-10-02 12:37:16 -0400

    Updated with errors removed

--- website/validate.inc.php
+++ website/validate.inc.php
@@ -3,7 +3,7 @@ require_once('database.php');
 $emailAddress = $_POST['emailAddress'];
 $password = $_POST['password'];
 $query = "SELECT firstName, lastName, pronouns FROM teaandCoffeeAccessoriesManagers " .
-        "WHERE emailAddress = ? AND password = SHA2(?,64)";
+        "WHERE emailAddress = ? AND password = SHA2(?,256)";
 $db = getDB();
 $stmt = $db->prepare($query);
 $stmt->bind_param("ss", $emailAddress, $password);
@@ -15,10 +15,10 @@ if ($fetched && isset($name)) {
    echo "<h2>Welcome to Inventory Helper of Tea and Coffee Accessories Shop, $name</h2>\n";
    $_SESSION['login'] = true;
 
-$_SESSION['emailAddress'] = $emailAddress;
-$_SESSION['firstName'] = $firstName;
-$_SESSION['lastName'] = $lastName;
-$_SESSION['pronouns'] = $pronouns;
+   $_SESSION['emailAddress'] = $emailAddress;
+   $_SESSION['firstName'] = $firstName;
+   $_SESSION['lastName'] = $lastName;
+   $_SESSION['pronouns'] = $pronouns;
 header("Location: index.php");
 } else {
    echo "<h2>Sorry, login information was incorrect on Tea and Coffee Accessories Shop</h2>\n";
